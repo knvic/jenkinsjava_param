8ea16e61cdb0b5967e09bca015e87a38ea320992
Nikolay <nicamx@yandex.ru>
2023-09-13 20:27:51 +0300

    Добавили Allure

--- src/test/java/guru/qa/tests/BaseTest.java
+++ src/test/java/guru/qa/tests/BaseTest.java
@@ -1,6 +1,8 @@
 package guru.qa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
@@ -8,6 +10,7 @@ public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("Allure", new AllureSelenide());
         System.setProperty("selenide.browser", "Chrome");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1800";
